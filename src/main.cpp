351a4dd279b2959ab92a8623bc56a44ecc147d73
Sorab828 <sorab.gla_cs.aiml24@gla.ac.in>
2026-02-11 13:56:26 +0530

    create display object

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_SSD1306.h>>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+// oled 12c address
+#define OLED_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, rst_pin:-1);
 
 void setup() {
 // write your initialization code here
